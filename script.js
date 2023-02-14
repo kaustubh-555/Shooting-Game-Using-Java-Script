b32540804773bdd44d24efe8bf1246440299badb
kaustubh-555 <kaustubh517suroshi@gmail.com>
2023-02-14 23:40:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+let i;
+let difficulty=2000;
+function reposition(){
+    x=Math.random()*1000;
+    y=Math.random()*500;
+    target.style.left=`${x}px`
+    target.style.top=`${y}px`
+}
+let x=Math.random()*1000;
+let y= Math.random()*500;
+let target=document.createElement("div");
+target.id="tar";
+let img=document.createElement("img");
+img.setAttribute("src","./images/target.avif");
+target.appendChild(img);
+target.style.display="inline";
+target.style.position="relative";
+target.style.left=`${x}px`;
+target.style.right=`${y}px`;
+target.addEventListener('click',increasescore);
+let alredyPlaying=false;
+document.querySelector("#start").addEventListener('click',begin);
+document.querySelector('#stop').addEventListener('click',terminate);
+
+function begin(e){
+    if(alredyPlaying===false){
+        document.querySelector(".main").appendChild(target);
+        i=setInterval(reposition,2000);
+    }
+    else{
+        document.getElementById("score").innerHTML="0";
+        i=setInterval(reposition,2000);
+    }
+    e.preventDefault();
+}
+let lowerLimit=1000;
+function increasescore(e){
+    let score=document.getElementById('score').innerHTML;
+    let s=Number(score);
+    s+=100;
+    if(s>lowerLimit&&s<10000){
+        lowerLimit+=1000;
+        difficulty-=120;
+    }
+    document.getElementById('score').innerHTML=s;
+    clearInterval(i);
+    i=setInterval(reposition,2000);
+    reposition();
+}
+function terminate(){
+    clearInterval(i);
+    alredyPlaying=true;
+}    
